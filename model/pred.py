d7a311d049d8733faf4ef9c055e73cc1bb2e4ad4
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-06-22 17:01:49 +0530

    pred

--- model/pred.py
+++ model/pred.py
@@ -2,14 +2,16 @@ import json
 import pickle
 import numpy as np
 import torch
-import torch.nn
+import os
+import subprocess
 from pathlib import Path
+from rich import print
 
 ROOT = Path(__file__).resolve().parent.parent
 
 from model import SkillClassifier
 
-def predict(job_desc, role="", job_type="", top_k=20):
+def predict(job_desc, role="", job_type="", top_k=50):
     with open('prep/label_vocab.json') as f:
         label_vocab = json.load(f)
     with open('prep/vectorizer.pkl', 'rb') as f:
@@ -29,6 +31,12 @@ def predict(job_desc, role="", job_type="", top_k=20):
         
     ranked = sorted(zip(label_vocab, probs), key=lambda x: -x[1])
     return ranked[:top_k]
+
+def clear_console():
+    if os.name == 'nt':
+        subprocess.run('cls', shell=True)
+    else:
+        subprocess.run(['clear'])
     
 if __name__ == "__main__":
     jd = """
@@ -38,13 +46,23 @@ if __name__ == "__main__":
     with vector databases for retrieval-augmented generation and be
     comfortable calling REST APIs and Docker, Knowledge in Agents is a must.
     """
+    role = "AI Engineer"
+    type = "Junior"
     
-    results = predict(jd, role="AI Engineer", job_type="Internship")
+    clear_console()
+    
+    print("[yellow][JobAuto] ")
+    print(">> Welcome to JobAuto! \n")
+    jd = input("Enter Job Description : \n")
+    role = input("\nEnter Job role : \n")
+    type = input("\nEnter type : \n")
+    
+    results = predict(jd, role=role, job_type=type)
     
     print("\n Job Description Provided : \n")
     print(jd.strip())
     print("\n TOP Skills \n")
     for label, prob in results:
-        bar = '#' * int(prob * 30)
-        print(f" {label:25s} {prob:.2f}  {bar}")
+        bar = '█' * int(prob * 30)
+        print(f" {label:25s} {prob:.2f}  {bar}\n")
         
