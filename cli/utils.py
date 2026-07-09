5435763970da3389d20256488d86c80f220f0fb6
Aks_fuhrer <akshaysureshbabu200@gmail.com>
2026-07-09 07:09:29 +0530

    utils

---
+++ cli/utils.py
@@ -0,0 +1,33 @@
+import os
+import subprocess
+from pyfiglet import Figlet
+from rich import print
+
+def clear_console() -> None:
+    if os.name == "nt":
+        subprocess.run("cls", shell=True)
+    else:
+        subprocess.run(["clear"])
+
+
+def API_title() -> None:
+    f = Figlet(font="slant")
+    print("——————————————————————————————————————————————————————————————————————")
+    print(f.renderText("JobSelect CLI"))
+    print("           Copyright Akshay Babu, All rights reserved")
+    print("——————————————————————————————————————————————————————————————————————")
+
+
+def title() -> None:
+    f = Figlet(font="slant")
+    print("——————————————————————————————————————————————————————————————————————")
+    print(f.renderText("JobSelect CLI"))
+    print("[blue]               Running Model : JobAnalyze 6k v1.0")
+    print("           Copyright Akshay Babu, All rights reserved")
+    print("——————————————————————————————————————————————————————————————————————")
+
+
+def query(message: str) -> None:
+    print("\n[yellow][JobSelect]")
+    print(f"[yellow] >> {message} : ")
+    print("[You]")
