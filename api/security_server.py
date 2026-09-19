41393abb34dcb277373a13a2a0bbea91371bd1a2
Akshay_Babu <akshaysureshbabu200@gmail.com>
2026-09-19 18:40:40 +0530

    Limit CPU thread pools for low-memory deployment

--- api/security_server.py
+++ api/security_server.py
@@ -2,6 +2,13 @@
 
 import os
 
+# Keep CPU thread pools from multiplying memory usage on Render Free.
+os.environ.setdefault("TOKENIZERS_PARALLELISM", "false")
+os.environ.setdefault("OMP_NUM_THREADS", "1")
+os.environ.setdefault("MKL_NUM_THREADS", "1")
+os.environ.setdefault("OPENBLAS_NUM_THREADS", "1")
+os.environ.setdefault("NUMEXPR_NUM_THREADS", "1")
+
 import uvicorn
 
 from JobAnalyze_API import app
